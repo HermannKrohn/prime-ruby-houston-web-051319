452004026f7a7f3d749acc75711d93d020427786
Hermann Krohn <krohnhermannacosta@gmail.com>
2019-04-28 05:07:04 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(integer)
+  count = 0 
+  i = 0 
+  
+  while i < integer
+    if integer%i == 0 
+      count += 1 
+      if count == 3
+        return false
+      end
+      i += 1 
+    end
+  end
+  
